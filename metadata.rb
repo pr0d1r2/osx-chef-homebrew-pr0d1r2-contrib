7c38c7d77119c87290413e7e3572b1b744d4c13c
Marcin Nowicki <pr0d1r2@gmail.com>
2015-12-12 12:20:15 +0100

    Add recipe

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name             'homebrew-pr0d1r2-contrib'
+maintainer       'Marcin Nowicki'
+maintainer_email 'marcin@prodix.pl'
+license          'MIT'
+description      'Taps pr0d1r2/contrib'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
